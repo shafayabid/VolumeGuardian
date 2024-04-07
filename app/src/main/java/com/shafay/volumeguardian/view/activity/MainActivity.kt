4dc09800f92b65e99fb0ba331e404e1770241710
Shafay Abid <shafayabid4@gmail.com>
2024-04-07 23:34:48 +0500

    add: home fragment, nav graph

---
+++ app/src/main/java/com/shafay/volumeguardian/view/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.shafay.volumeguardian.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.shafay.volumeguardian.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
