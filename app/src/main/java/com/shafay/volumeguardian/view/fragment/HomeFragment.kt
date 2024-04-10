5a880049848013e4eff70ff9b1f1199d2860ffb5
Shafay Abid <shafayabid4@gmail.com>
2024-04-10 22:26:49 +0500

    add: volume change listener

--- app/src/main/java/com/shafay/volumeguardian/view/fragment/HomeFragment.kt
+++ app/src/main/java/com/shafay/volumeguardian/view/fragment/HomeFragment.kt
@@ -6,10 +6,12 @@ import android.app.Dialog
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.database.ContentObserver
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.media.AudioManager
 import android.os.Bundle
+import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -19,11 +21,12 @@ import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.shafay.volumeguardian.databinding.DialogueNoAudioBinding
 import com.shafay.volumeguardian.databinding.FragmentHomeBinding
-import com.shafay.volumeguardian.mediapipe.AudioClassifierHelper
 import com.shafay.volumeguardian.service.DetectAudioService
 
 
 class HomeFragment : Fragment() {
+
+    private val TAG = "HomeFragment"
 
     private val binding by lazy {
         FragmentHomeBinding.inflate(layoutInflater)
@@ -35,10 +38,10 @@ class HomeFragment : Fragment() {
         ) { isGranted: Boolean ->
             if (isGranted) {
                 Toast.makeText(context, "Permission request granted", Toast.LENGTH_LONG).show()
-                if(audioManager.isMusicActive){
+                if (audioManager.isMusicActive) {
                     activity?.startService(Intent(activity, DetectAudioService::class.java))
-                }else{
-                    context?.let{ context ->
+                } else {
+                    context?.let { context ->
                         activity?.let { activity ->
                             showNoMusicDialogue(context, activity)
                         }
@@ -56,6 +59,11 @@ class HomeFragment : Fragment() {
 
         context?.let {
             audioManager = it.getSystemService(Context.AUDIO_SERVICE) as AudioManager
+            checkAudio()
+        }
+
+        activity?.let {
+            initAudioListener(it)
         }
 
     }
@@ -76,7 +84,11 @@ class HomeFragment : Fragment() {
                     checkAndRequestAudioPermission(context, activity)
                 }
             }
+            binding.ivSpeakerDisabled.setOnClickListener {
+                Toast.makeText(context, "Please Raise Music Volume!", Toast.LENGTH_SHORT).show()
+            }
         }
+
 
     }
 
@@ -129,4 +141,53 @@ class HomeFragment : Fragment() {
         dialogue.show()
     }
 
+    private fun initAudioListener(activity: Activity) {
+        val mSettingsContentObserver = SettingsContentObserver(Handler())
+        activity.applicationContext.contentResolver.registerContentObserver(
+            android.provider.Settings.System.CONTENT_URI,
+            true,
+            mSettingsContentObserver
+        );
+    }
+
+    private fun checkAudio() {
+        if (audioManager.getStreamVolume(AudioManager.STREAM_MUSIC) == 0) {
+            showDisabledButton()
+        } else {
+            showEnabledButton()
+        }
+    }
+
+    private fun showEnabledButton() {
+        binding.ivSpeaker.visibility = View.VISIBLE
+        binding.ivSpeakerDisabled.visibility = View.GONE
+
+        binding.tvActivateVolume.visibility = View.VISIBLE
+        binding.tvDisabledVolume.visibility = View.GONE
+        binding.tvRaiseVolume.visibility = View.GONE
+    }
+
+    private fun showDisabledButton() {
+        binding.ivSpeaker.visibility = View.GONE
+        binding.ivSpeakerDisabled.visibility = View.VISIBLE
+
+        binding.tvActivateVolume.visibility = View.GONE
+        binding.tvDisabledVolume.visibility = View.VISIBLE
+        binding.tvRaiseVolume.visibility = View.VISIBLE
+    }
+
+
+    inner class SettingsContentObserver(handler: Handler?) : ContentObserver(handler) {
+        override fun deliverSelfNotifications(): Boolean {
+            return super.deliverSelfNotifications()
+        }
+
+        override fun onChange(selfChange: Boolean) {
+            super.onChange(selfChange)
+            checkAudio()
+        }
+    }
+
 }
+
+
